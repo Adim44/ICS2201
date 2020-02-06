e8aad954cb2df92f39634b3f37c9f2cb6a0ca5ee
envy <njuejohn@gmail.com>
2020-02-06 10:08:05 +0300

    Initial

---
+++ src/MyNameAndAge.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class MyNameAndAge {
+	public static void main(String args[]) throws IOException {
+		InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(inputStreamReader);
+		String name;
+		int age;
+		System.out.print("Enter your name: ");
+		name = br.readLine();
+		
+
+		System.out.print("Enter your age: ");
+		age = Integer.parseInt(br.readLine());
+		
+		
+		System.out.print("Hello, "+name+ " your age is "+age);
+	}
+}
